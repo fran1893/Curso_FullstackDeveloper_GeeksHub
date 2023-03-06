640ab3f3bbad7a939226c5b682d88080e93a8877
Francisco Diaz <diazsalerno2@outlook.es>
2023-03-06 13:15:02 +0100

    Javascript first notes

---
+++ Javascript/Ejercicio1/js/main.js
@@ -0,0 +1,19 @@
+// Comentario simple
+
+/*
+esto es un comentario multilinea
+*/
+console.log("Hola mundo de nuevo en JavaScript");
+
+let edad_1 = 30;
+let miEdad = 29;
+let miNombre = "Bob";
+let a;
+
+miEdad = "Alice";
+
+let miMensaje = 'Esto es una cadena con comillas simples';
+
+console.log(miMensaje);
+// variable 'a' tiene valor undefined
+console.log(a);
