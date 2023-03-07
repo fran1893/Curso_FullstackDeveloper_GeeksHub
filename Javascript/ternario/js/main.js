9283e83e2f6b23e0b456080b9fdc6fe4355c1929
Francisco Diaz <diazsalerno2@outlook.es>
2023-03-07 12:33:31 +0100

    Added operador ternario notes

---
+++ Javascript/ternario/js/main.js
@@ -0,0 +1,10 @@
+let edad = 18;
+let verPeliculaTerror;
+// Opción 1
+if (edad >= 18) {
+    verPeliculaTerror = 'SI';
+} else {
+    verPeliculaTerror = 'NO';
+}
+// Opción 2
+verPeliculaTerror = edad >= 18 ? 'SI' : 'NO'
