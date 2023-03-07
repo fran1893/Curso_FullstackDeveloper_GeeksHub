8726934dc35c6f01dbfc6e79aecce9402ec7a9ba
Francisco Diaz <diazsalerno2@outlook.es>
2023-03-07 12:28:15 +0100

    Add switch option to kata2 javascript exercise

--- Javascript/kata2/js/main.js
+++ Javascript/kata2/js/main.js
@@ -20,21 +20,52 @@ let nota = +prompt("Díme una nota del 0 al 10 y te devolvere su calificación")
 let esNotaValida = Number.isInteger(nota) && nota >= 0 && nota <= 10; // Positiva
 let esNotaNoValida = !Number.isInteger(nota) || nota < 0 || nota > 10; // Negativa
 
-console.log(nota)
-// CLASIFICACIÓN
 
+// CLASIFICACIÓN USANDO IF
+
+// if (!esNotaValida) {
+//     message = 'La nota debe ser entre 0 y 10, sin decimales'
+// }
+// else if (nota <= 4) {
+//     message = 'La calificación de esa nota es Insuficiente';
+// } else if (nota <= 6) {
+//     message = 'La calificación de esa nota es Suficiente';
+// } else if (nota <= 8) {
+//     message = 'La calificación de esa nota es Notable';
+// } else {
+//     message = 'La calificación de esa nota es Sobresaliente';
+// }
+
+
+
+
+
+// CLASIFICACIÓN USANDO SWITCH
 if (!esNotaValida) {
     message = 'La nota debe ser entre 0 y 10, sin decimales'
-}
-else if (nota <= 4) {
-    message = 'La calificación de esa nota es Insuficiente';
-} else if (nota <= 6) {
-    message = 'La calificación de esa nota es Suficiente';
-} else if (nota <= 8) {
-    message = 'La calificación de esa nota es Notable';
 } else {
-    message = 'La calificación de esa nota es Sobresaliente';
+    switch (nota) {
+        case 0:
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+            message = "Tienes un insuficiente";
+            break;
+        case 5:
+        case 6:
+            message = "Tienes un suficiente";
+            break;
+        case 7:
+        case 8:
+            message = "Tienes un notable";
+            break;
+        default:
+            message = "Tienes un sobresaliente";
+    }
 }
+
+
 
 // VISUALIZAR RESULTADO
 alert(message)
