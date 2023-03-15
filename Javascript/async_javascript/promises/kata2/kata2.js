4b5c69f9dc243a96331a34169b33dd631151e1fe
Francisco Diaz <diazsalerno2@outlook.es>
2023-03-15 13:18:49 +0100

    Add pirinting on html with kata2 exercise

--- Javascript/async_javascript/promises/kata2/kata2.js
+++ Javascript/async_javascript/promises/kata2/kata2.js
@@ -1,7 +1,9 @@
 // Crea una función que reciba 1 número y devuelva una promesa que se
 // resuelve tras 1 segundo si el número es par y se rechace si es impar.
 
-let numAcomparar = 2;
+let input = document.getElementById("number");
+let boton = document.getElementById("activate");
+let printResult = document.getElementById("result");
 
 const parImpar = (num) => {
   return new Promise((resolve, reject) => {
@@ -15,10 +17,13 @@ const parImpar = (num) => {
   });
 };
 
-parImpar(numAcomparar)
-  .then((response) => {
-    console.log(response);
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
+boton.addEventListener("click", () => {
+  parImpar(+input.value)
+    .then((response) => {
+      printResult.innerHTML = response;
+    })
+    .catch((err) => {
+      printResult.innerHTML = err.message;
+      console.log(err.message);
+    });
+});
