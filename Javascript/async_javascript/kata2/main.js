da95ddb8853370b2d6ab33418fed86dbaa8c6903
Francisco Diaz <diazsalerno2@outlook.es>
2023-03-15 09:04:55 +0100

    Async function print name exercise

--- Javascript/async_javascript/kata2/main.js
+++ Javascript/async_javascript/kata2/main.js
@@ -9,3 +9,19 @@ const printSuma = (num1, num2) => {
 setTimeout(() => {
   printSuma(2, 2);
 }, 1000);
+
+// const number1 = 2;
+// const number2 = 4;
+
+// const sumar = (a, b, cb) => {
+//   const resul = a + b;
+//   cb(resul);
+// };
+
+// const callback = (r) => {
+//   console.log(r);
+// };
+
+// setTimeout(() => {
+//   sumar(number1, number2, callback());
+// }, 1000);
