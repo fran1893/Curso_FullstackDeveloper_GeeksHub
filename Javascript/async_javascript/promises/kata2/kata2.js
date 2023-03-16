d36659e0ba17380235268571c960bd347a63c0eb
Francisco Diaz <diazsalerno2@outlook.es>
2023-03-16 10:37:02 +0100

    Add async await form to 'par o impar' exercise on kata2 promises folder

--- Javascript/async_javascript/promises/kata2/kata2.js
+++ Javascript/async_javascript/promises/kata2/kata2.js
@@ -1,10 +1,12 @@
 // Crea una función que reciba 1 número y devuelva una promesa que se
 // resuelve tras 1 segundo si el número es par y se rechace si es impar.
 
+// Declaración de variables
 let input = document.getElementById("number");
 let boton = document.getElementById("activate");
 let printResult = document.getElementById("result");
 
+// Creación de la promesa
 const parImpar = (num) => {
   return new Promise((resolve, reject) => {
     if (num % 2 == 0) {
@@ -17,17 +19,23 @@ const parImpar = (num) => {
   });
 };
 
+// Crear funcion async-await
+const async_parImpar = async () => {
+  try {
+    const showResult = await parImpar(+input.value);
+    printResult.innerHTML = showResult;
+  } catch (error) {
+    printResult.innerHTML = err.message;
+  }
+};
+
+// Llamamos a la funcion async-await al hacer click el boton del html
 boton.addEventListener("click", () => {
-  parImpar(+input.value)
-    .then((response) => {
-      printResult.innerHTML = response;
-    })
-    .catch((err) => {
-      printResult.innerHTML = err.message;
-      console.log(err.message);
-    });
+  async_parImpar();
 });
 
+// Funcion para borrar el resultado
+// que se muestra en el parrafo cuando se vuelve a escribir nuevamente
 const borrarParrafo = () => {
   printResult.innerHTML = "";
 };
