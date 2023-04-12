a44feade061796243b46727b1d859dce66b34e51
Francisco Diaz <diazsalerno2@outlook.es>
2023-04-12 19:36:28 +0200

    Update db.js www and config.json on sequelize alumnos exercise

--- nodejs/server_express/sequelize/db/db.js
+++ nodejs/server_express/sequelize/db/db.js
@@ -1,5 +1,4 @@
 const config = require("../config/config.json");
-
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(
@@ -8,24 +7,22 @@ const sequelize = new Sequelize(
   process.env.MYSQL_PASSWORD || config.development.password,
   {
     host: process.env.MYSQL_HOST || config.development.host,
-    port: process.env.MYSQL_PORT || config.development.port,
+    port: process.env.MYSQL_PORT || config.development.port || 3306,
     dialect: "mysql",
-    // operatorAliases: false,
-    // pool: {
-    //   max: 5,
-    //   min: 0,
-    //   acquire: 30000,
-    //   idle: 10000,
-    // },
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
   }
 );
 
-module.exports = sequelize.authenticate().then((db) => {
-  console.log(
-    `MYSQL connected to ${
-      process.env.MYSQL_DATABASE || config.development.database
-    }`
-  );
-
-  return db;
-});
+module.exports = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection to database has been established successfully");
+  } catch (error) {
+    console.error("Unable to connect to database: ", error);
+  }
+};
